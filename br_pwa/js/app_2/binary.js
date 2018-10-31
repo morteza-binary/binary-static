1e824aacd7fd22f03bc40a4725c94e76595e8462
Morteza Tavanarad <morteza@Morteza-MacBook.local>
2018-10-31 17:42:45 +0800

    Deploy to pwa

--- br_pwa/js/app_2/binary.js
+++ br_pwa/js/app_2/binary.js
@@ -136,7 +136,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/br_pwa/js/";
+/******/ 	__webpack_require__.p = "/br_pwa/js/app_2/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
